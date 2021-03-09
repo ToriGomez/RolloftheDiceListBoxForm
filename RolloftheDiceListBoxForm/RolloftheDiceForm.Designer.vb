d624d0fffd581c36fd65c27907adccd536501a33
Victoria Gomez <77286079+ToriGomez@users.noreply.github.com>
2021-03-09 12:01:55 -0700

    Add project files.

---
+++ RolloftheDiceListBoxForm/RolloftheDiceForm.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RolloftheDiceForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.MenuTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RollTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RolloftheDiceListBox = New System.Windows.Forms.ListBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.HiddenDisplayTextBox = New System.Windows.Forms.TextBox()
+        Me.TopMenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TopMenuStrip
+        '
+        Me.TopMenuStrip.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.TopMenuStrip.ImageScalingSize = New System.Drawing.Size(40, 40)
+        Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuTopMenuItem, Me.HelpTopMenuItem})
+        Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.TopMenuStrip.Name = "TopMenuStrip"
+        Me.TopMenuStrip.Size = New System.Drawing.Size(1124, 49)
+        Me.TopMenuStrip.TabIndex = 4
+        Me.TopMenuStrip.Text = "MenuStrip1"
+        '
+        'MenuTopMenuItem
+        '
+        Me.MenuTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RollTopMenuItem, Me.ClearTopMenuItem, Me.ExitTopMenuItem})
+        Me.MenuTopMenuItem.Name = "MenuTopMenuItem"
+        Me.MenuTopMenuItem.Size = New System.Drawing.Size(119, 45)
+        Me.MenuTopMenuItem.Text = "&Menu"
+        '
+        'RollTopMenuItem
+        '
+        Me.RollTopMenuItem.Name = "RollTopMenuItem"
+        Me.RollTopMenuItem.Size = New System.Drawing.Size(448, 54)
+        Me.RollTopMenuItem.Text = "&Roll"
+        '
+        'ClearTopMenuItem
+        '
+        Me.ClearTopMenuItem.Name = "ClearTopMenuItem"
+        Me.ClearTopMenuItem.Size = New System.Drawing.Size(448, 54)
+        Me.ClearTopMenuItem.Text = "&Clear"
+        '
+        'ExitTopMenuItem
+        '
+        Me.ExitTopMenuItem.Name = "ExitTopMenuItem"
+        Me.ExitTopMenuItem.Size = New System.Drawing.Size(448, 54)
+        Me.ExitTopMenuItem.Text = "&Exit"
+        '
+        'HelpTopMenuItem
+        '
+        Me.HelpTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutTopMenuItem})
+        Me.HelpTopMenuItem.Name = "HelpTopMenuItem"
+        Me.HelpTopMenuItem.Size = New System.Drawing.Size(104, 45)
+        Me.HelpTopMenuItem.Text = "&Help"
+        '
+        'AboutTopMenuItem
+        '
+        Me.AboutTopMenuItem.Name = "AboutTopMenuItem"
+        Me.AboutTopMenuItem.Size = New System.Drawing.Size(448, 54)
+        Me.AboutTopMenuItem.Text = "&About"
+        '
+        'RolloftheDiceListBox
+        '
+        Me.RolloftheDiceListBox.FormattingEnabled = True
+        Me.RolloftheDiceListBox.ItemHeight = 31
+        Me.RolloftheDiceListBox.Location = New System.Drawing.Point(56, 106)
+        Me.RolloftheDiceListBox.Name = "RolloftheDiceListBox"
+        Me.RolloftheDiceListBox.Size = New System.Drawing.Size(978, 407)
+        Me.RolloftheDiceListBox.TabIndex = 1
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(203, 540)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(249, 156)
+        Me.RollButton.TabIndex = 0
+        Me.RollButton.Text = "&Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(495, 540)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(249, 156)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(785, 540)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(249, 156)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "&Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'HiddenDisplayTextBox
+        '
+        Me.HiddenDisplayTextBox.Location = New System.Drawing.Point(12, 540)
+        Me.HiddenDisplayTextBox.Name = "HiddenDisplayTextBox"
+        Me.HiddenDisplayTextBox.Size = New System.Drawing.Size(100, 38)
+        Me.HiddenDisplayTextBox.TabIndex = 5
+        '
+        'RolloftheDiceForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1124, 769)
+        Me.Controls.Add(Me.HiddenDisplayTextBox)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.RolloftheDiceListBox)
+        Me.Controls.Add(Me.TopMenuStrip)
+        Me.MainMenuStrip = Me.TopMenuStrip
+        Me.Name = "RolloftheDiceForm"
+        Me.Text = "Roll of the Dice"
+        Me.TopMenuStrip.ResumeLayout(False)
+        Me.TopMenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TopMenuStrip As MenuStrip
+    Friend WithEvents MenuTopMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpTopMenuItem As ToolStripMenuItem
+    Friend WithEvents RollTopMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearTopMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitTopMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutTopMenuItem As ToolStripMenuItem
+    Friend WithEvents RolloftheDiceListBox As ListBox
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents HiddenDisplayTextBox As TextBox
+End Class
---
+++ RolloftheDiceListBoxForm/RolloftheDiceForm.vb
@@ -0,0 +1,91 @@
+﻿Option Explicit On
+Option Strict On
+Public Class RolloftheDiceForm
+    Private Sub RolloftheDiceForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        HiddenDisplayTextBox.Enabled = False
+    End Sub
+
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim rollsResult As Integer
+        Dim diceValue As Integer = 1
+        Dim numbers(12) As Integer
+        Dim display(4) As String
+        Dim title As String = (vbTab & vbTab & vbTab & "Roll of the Dice")
+        Dim dashRow As String = (StrDup(84, " - "))
+        Dim diceValueTotal As String
+
+
+        RolloftheDiceListBox.Items.Clear()
+        'Generates the sum of two dice rolled values. Min of 2, Max of 12.
+        'Accumulates the amount of times each value is rolled from 1000 rolls.
+        For i = 1 To 1000
+            rollsResult = RandomNumber()
+            Select Case rollsResult
+                Case 2
+                    numbers(2) += diceValue
+                Case 3
+                    numbers(3) += diceValue
+                Case 4
+                    numbers(4) += diceValue
+                Case 5
+                    numbers(5) += diceValue
+                Case 6
+                    numbers(6) += diceValue
+                Case 7
+                    numbers(7) += diceValue
+                Case 8
+                    numbers(8) += diceValue
+                Case 9
+                    numbers(9) += diceValue
+                Case 10
+                    numbers(10) += diceValue
+                Case 11
+                    numbers(11) += diceValue
+                Case 12
+                    numbers(12) += diceValue
+                Case Else
+                    MsgBox($"Error!: {rollsResult}")
+            End Select
+        Next
+
+        For i = 2 To UBound(numbers)
+            diceValueTotal = ($"{numbers(i)}|".PadLeft(4) & vbTab)
+        Next
+
+        display(0) = title
+        display(1) = dashRow
+        display(2) = DiceValueDisplay()
+        display(3) = dashRow
+        display(4) = diceValueTotal
+
+        'CancatinateDisplay(title, dashRow, DiceValueDisplay())
+        RolloftheDiceListBox.Items.Add(display(0) & display(1))
+        RolloftheDiceListBox.EndUpdate()
+    End Sub
+    'Function CancatinateDisplay(title As String, spacing As String, value As String _
+    '                             & valuetotal As String) As String
+    '    Return title + spacing
+    'End Function
+    Function DiceValueDisplay() As String
+        Dim countDisplay As String
+        For i = 2 To 12
+            countDisplay = (($"{i}|").PadLeft(4) & vbTab)
+        Next
+        Return countDisplay
+    End Function
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        RolloftheDiceListBox.Items.Clear()
+    End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    'Random value of roll of two dice
+    Function RandomNumber() As Integer
+        Dim value As Integer
+        Randomize()
+        value = (CInt((5 * Rnd()) + 1) + CInt((5 * Rnd()) + 1))
+        Return value
+    End Function
+
+End Class
